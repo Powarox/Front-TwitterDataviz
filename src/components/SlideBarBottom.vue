760b02a567713fdcaf05eedc702fb9353a3361eb
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-11 21:36:32 +0100

    Create component SlideBarBottom

---
+++ src/components/SlideBarBottom.vue
@@ -0,0 +1,63 @@
+<template lang="html">
+    <div id="slideBarBottom">
+        <div class="items">
+            <i class="fas fa-jedi"></i>
+            <div class="content">
+                <span>du text long</span>
+                <p>54 672 $</p>
+            </div>
+        </div>
+
+        <div class="items">
+            <i class="fab fa-old-republic"></i>
+            <div class="content">
+                <span>du text long</span>
+                <p>1877</p>
+            </div>
+        </div>
+
+        <div class="items">
+            <i class="fab fa-galactic-republic"></i>
+            <div class="content">
+                <span>du text long</span>
+                <p>445 %</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SlideBarBottom',
+    }
+</script>
+
+<style lang="css" scoped>
+    #slideBarBottom {
+        width: 90%;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .items {
+        display: flex;
+        align-items: center;
+    }
+
+    .items i {
+        margin: 10px;
+        padding: 9px;
+        color: #EC8294;
+        background: #FFF;
+        border-radius: 10px;
+        font-size: 23px;
+    }
+
+    .items .content {
+        font-size: 15px;
+    }
+
+    .items .content span {
+        color: #696B69;
+    }
+</style>
