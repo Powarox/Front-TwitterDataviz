7c34b7a4d2d560833379bb200ddd351147b062eb
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-11 21:36:06 +0100

    Create component SlideBarRight

---
+++ src/components/SlideBarRight.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+    <div id="slideBarRight">
+        <div class="slide">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  laborum.</p>
+        </div>
+        <div class="slide">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim d minim veniam, quis nostrud  laborum.</p>
+        </div>
+        <div class="slide">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim d minim veniam, quis nostrud  laborum.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SlideBarRight',
+    }
+</script>
+
+<style lang="css" scoped>
+    #slideBarRight {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .slide {
+        padding: 10px 20px;
+        background: #FFF;
+        box-shadow: 5px 10px 20px #D9D5EC;
+        border-radius: 10px;
+    }
+</style>
