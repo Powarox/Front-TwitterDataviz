3b73a67c0637ec5477c4cafd6c8baffd2f3e12be
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-14 18:05:46 +0100

    Update ApexRadial Component

--- src/components/charts/ApexRadialChart.vue
+++ src/components/charts/ApexRadialChart.vue
@@ -9,20 +9,14 @@
         name: 'ApexRadialChart',
         props: ['id', 'data', 'height'],
         mounted() {
-            console.log(this.data);
-
             let like = this.data.Like;
             let retweet = this.data.Retweet;
             let comment = this.data.ReplyCount;
             let quote = this.data.TweetQuote;
-
-            let count = like + retweet + comment + quote;
-
-            console.log(like, retweet, comment, quote);
+            let total = like + retweet + comment + quote;
 
             let options = {
-                // series: [quote, comment, retweet, like],
-                series: [50, 50, 50, 50],
+                series: [like*100/total, retweet*100/total, comment*100/total, quote*100/total],
                 chart: {
                     height: this.height,
                     type: 'radialBar',
@@ -40,13 +34,13 @@
                                 show: true,
                                 label: 'Total',
                                 formatter: function() {
-                                    return count.toLocaleString('fr-FR');
+                                    return total.toLocaleString('fr-FR');
                                 }
                             }
                         }
                     }
                 },
-                labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+                labels: ['Like', 'Retweet', 'comment', 'quote'],
             };
 
             let chart = new ApexCharts(document.querySelector("#" + this.id), options);
