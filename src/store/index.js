67b318ea5d71133697a599243a98919239051e49
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-15 22:07:05 +0100

    Add dataset2 & dataset3

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import {createStore} from 'vuex';
 
 import dataset1 from './dataset1';
+import dataset2 from './dataset2';
+import dataset3 from './dataset3';
 
 export const store = createStore({
     debug : true,
     modules: {
         dataset1,
+        dataset2,
+        dataset3,
       },
  })
