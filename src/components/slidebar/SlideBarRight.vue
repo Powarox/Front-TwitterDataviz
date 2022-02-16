f27522755c5cde1220bd58881e5d55a2e3ee30b7
Powarox219 <moriniere.robin1877@gmail.com>
2022-02-17 00:03:44 +0100

    Update

--- src/components/slidebar/SlideBarRight.vue
+++ src/components/slidebar/SlideBarRight.vue
@@ -6,11 +6,6 @@
             <div id="apexRightSlideChart1">
                 <ApexRadialChart :id="'apexRightSlideChart1'" :choose="'Retweet'" :data="dataRadial" :height="230" :font-size="14"/>
             </div>
-
-            <!-- <div class="item" v-for="(item, id) in this.dataRadial" v-bind:key="item.Tweet">
-                <p>Tweet {{ id }}</p>
-                <p>{{ item.Tweet }}</p>
-            </div> -->
         </div>
 
         <div class="slide info">
@@ -25,18 +20,6 @@
                 <ApexLineChart :id="'apexRightSlideChart2'" :data="dataRadial" :height="210"/>
             </div>
         </div>
-
-        <!-- <div class="slide disclaimer special">
-            <p>All the data present in this application is public and comes directly from the official Twitter accounts of the players.</p>
-            <div class="bottomBar"></div>
-            <p>They were retrieved through a Python script using the Snscrape library which is used to crawl data directly from Twitter.</p>
-        </div>
-        <div class="slide disclaimer special">
-            <p>All the data present in this application is public and comes directly from the official Twitter accounts of the players.</p>
-        </div>
-        <div class="slide disclaimer special">
-            <p>They were retrieved through a Python script using the Snscrape library which is used to crawl data directly from Twitter.</p>
-        </div> -->
     </div>
 </template>
 
